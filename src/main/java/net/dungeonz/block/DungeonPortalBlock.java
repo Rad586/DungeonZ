c89a9af52a0a9babf7222bd86f404b7a471c2d1b
Globox1997 <mitschke11@web.de>
2024-05-27 18:18:04 +0200

    bigger portal feature

--- src/main/java/net/dungeonz/block/DungeonPortalBlock.java
+++ src/main/java/net/dungeonz/block/DungeonPortalBlock.java
@@ -1,5 +1,7 @@
 package net.dungeonz.block;
 
+import java.util.Iterator;
+
 import org.jetbrains.annotations.Nullable;
 
 import net.dungeonz.DungeonzMain;
@@ -43,9 +45,12 @@ public class DungeonPortalBlock extends BlockWithEntity {
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (player.getWorld().getBlockEntity(pos) != null && player.getWorld().getBlockEntity(pos) instanceof DungeonPortalEntity) {
-            DungeonPortalEntity dungeonPortalEntity = (DungeonPortalEntity) player.getWorld().getBlockEntity(pos);
+        if (player.getWorld().getBlockEntity(pos) != null && player.getWorld().getBlockEntity(pos) instanceof DungeonPortalEntity dungeonPortalEntity) {
 
+            if (isOtherDungeonPortalBlockNearby(world, pos)) {
+                dungeonPortalEntity = getMainDungeonPortalEntity(world, pos);
+                pos = getMainDungeonPortalBlockPos(world, pos);
+            }
             if (player.isCreativeLevelTwoOp() && (dungeonPortalEntity.getDungeon() == null || player.isSneaking())) {
                 if (!world.isClient()) {
                     DungeonServerPacket.writeS2COpenOpScreenPacket((ServerPlayerEntity) player, dungeonPortalEntity, null);
@@ -61,7 +66,6 @@ public class DungeonPortalBlock extends BlockWithEntity {
                 return ActionResult.success(world.isClient());
             }
         }
-
         return super.onUse(state, world, pos, player, hand, hit);
     }
 
@@ -69,6 +73,9 @@ public class DungeonPortalBlock extends BlockWithEntity {
     public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
         if (!world.isClient() && !entity.hasVehicle() && !entity.hasPassengers() && entity.canUsePortals() && entity instanceof ServerPlayerEntity) {
             if (!entity.hasPortalCooldown()) {
+                if (isOtherDungeonPortalBlockNearby(world, pos)) {
+                    pos = getMainDungeonPortalBlockPos(world, pos);
+                }
                 DungeonHelper.teleportDungeon((ServerPlayerEntity) entity, pos, entity.getUuid());
                 entity.resetPortalCooldown();
             }
@@ -79,6 +86,55 @@ public class DungeonPortalBlock extends BlockWithEntity {
     @Nullable
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
         return DungeonGateBlock.checkType(type, BlockInit.DUNGEON_PORTAL_ENTITY, world.isClient() ? DungeonPortalEntity::clientTick : DungeonPortalEntity::serverTick);
+    }
+
+    public static boolean isOtherDungeonPortalBlockNearby(World world, BlockPos pos) {
+        Iterator<BlockPos> iterator = BlockPos.iterateOutwards(pos, 1, 1, 1).iterator();
+        while (iterator.hasNext()) {
+            BlockPos checkPos = iterator.next();
+            if (checkPos.equals(pos)) {
+                continue;
+            }
+            if (world.getBlockState(checkPos).isOf(BlockInit.DUNGEON_PORTAL)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Nullable
+    public static BlockPos getMainDungeonPortalBlockPos(World world, BlockPos pos) {
+        BlockPos checkPos = new BlockPos(pos);
+        for (int i = 1; i < 30; i++) {
+            if (world.getBlockState(checkPos.east(1)).isOf(BlockInit.DUNGEON_PORTAL)) {
+                checkPos = checkPos.east(1);
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; i < 30; i++) {
+            if (world.getBlockState(checkPos.south(1)).isOf(BlockInit.DUNGEON_PORTAL)) {
+                checkPos = checkPos.south(1);
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; i < 30; i++) {
+            if (world.getBlockState(checkPos.down(1)).isOf(BlockInit.DUNGEON_PORTAL)) {
+                checkPos = checkPos.down(1);
+            } else {
+                break;
+            }
+        }
+        return world.getBlockEntity(checkPos) instanceof DungeonPortalEntity dungeonPortalEntity ? dungeonPortalEntity.getPos() : null;
+    }
+
+    @Nullable
+    public static DungeonPortalEntity getMainDungeonPortalEntity(World world, BlockPos pos) {
+        if (getMainDungeonPortalBlockPos(world, pos) != null) {
+            return (DungeonPortalEntity) world.getBlockEntity(getMainDungeonPortalBlockPos(world, pos));
+        }
+        return null;
     }
 
 }
